fb74dbcfa68154a218b62ba105038b52848dec59
Durgesh9788 <52974160+Durgesh9788@users.noreply.github.com>
2022-06-30 11:52:27 +0530

    Code update

---
+++ ui.apps/src/main/content/jcr_root/apps/myprojectsite/clientlibs/clientlib-react/resources/precache-manifest.087b0190e1f379d272f55939bb61199a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972b487747c6c6509c0b77147490eea4",
+    "url": "/etc.clientlibs/myprojectsite/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "6a52901e8f9ccfd44b04",
+    "url": "/etc.clientlibs/myprojectsite/clientlibs/clientlib-react/resources/static/css/main.2a17b812.chunk.css"
+  },
+  {
+    "revision": "7ec90da226bd7725773d",
+    "url": "/etc.clientlibs/myprojectsite/clientlibs/clientlib-react/resources/static/js/2.e9a943bf.chunk.js"
+  },
+  {
+    "revision": "167917ef3152685504ed6937f1e7745e",
+    "url": "/etc.clientlibs/myprojectsite/clientlibs/clientlib-react/resources/static/js/2.e9a943bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a52901e8f9ccfd44b04",
+    "url": "/etc.clientlibs/myprojectsite/clientlibs/clientlib-react/resources/static/js/main.0fbb6534.chunk.js"
+  },
+  {
+    "revision": "4b51aaa81b0adcbda493",
+    "url": "/etc.clientlibs/myprojectsite/clientlibs/clientlib-react/resources/static/js/runtime-main.c9e7016f.js"
+  }
+]);
